bb89f1e482ea3e21737f820e9a6f4f81379f5691
Huy Lưu <huylv@hopnhat.vn>
2020-09-18 16:03:17 +0700

    add mongoose

--- bin/server.js
+++ bin/server.js
@@ -4,8 +4,20 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
-
 const router = express.Router();
+const mongoose = require("mongoose");
+const keys = require('../src/config/key')
+const apiRouter = require('../src/routes/api')
+const postRouter = require('../src/routes/post')
+
+mongoose.set('useCreateIndex', true)
+mongoose.connect(keys.mongodb.dbURL, {
+  useNewUrlParser: true, useUnifiedTopology: true,
+}, () => {
+  console.log('connet monggodb')
+});
+
+
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Hello from Express.js!</h1>');
@@ -13,7 +25,7 @@ router.get('/', (req, res) => {
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
-
+app.use(apiRouter, postRouter);
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
